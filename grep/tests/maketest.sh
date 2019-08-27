118c29c773ebeaeeff398308a27887986dae5a53
Alex Dikelsky <alexander.dikelsky@gmail.com>
2019-08-27 18:02:09 -0500

    Added some sed info

--- grep/tests/maketest.sh
+++ grep/tests/maketest.sh
@@ -1,22 +1,23 @@
-    #!/bin/bash 
-    
-    
-    function make_string {
-        let n=0
-        final_string=""
-        while [ $n -lt 100 ]
-        do
-            final_string+=$RANDOM
-            n=$(echo $n + 1 | bc)
-        done
-        echo $final_string;
-    }
-    
-    
-    let i=0
-    while [ $i -lt 10 ]
+#!/bin/bash 
+
+
+function make_string {
+    let n=0
+    final_string=""
+    while [ $n -lt 100 ]
     do
-        make_string > testfile$(echo $i).txt
-        i=$(echo $i + 1 | bc)
-        
+        final_string+=$RANDOM
+        n=$(echo $n + 1 | bc)
     done
+    echo $final_string;
+}
+
+
+let i=0
+while [ $i -lt 10 ]
+do
+    echo $i
+    make_string > testfile$(echo $i).txt
+    i=$(echo $i + 1 | bc)
+    
+done
